b4b9a1c96ddb07aca35060b2b3b9903a05fc74df
TomXemmings <tomxemmings@gmail.com>
2025-07-20 23:08:41 +0500

    Fix script

--- app/Jobs/FetchYandexCookies.php
+++ app/Jobs/FetchYandexCookies.php
@@ -80,6 +80,7 @@ class FetchYandexCookies implements ShouldQueue
 
             # Password
             $this->waitForSelector($page, 'button[id="passp:sign-in"]')->click();
+            $page->keyboard()->press('Enter');
             sleep(5);
 
             $this->waitForSelector($page, '#passp-field-passwd', 30000)->click();
